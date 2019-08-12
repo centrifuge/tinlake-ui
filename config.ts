2ed369d6b6071166d8ee9c43fecdf7db78b6c6af
ilinzweilin <ilinzweilin@ilinzweilins-MacBook-Pro.local>
2019-08-12 17:08:44 +0200

    pass transaction timeout through env

--- config.ts
+++ config.ts
@@ -4,7 +4,8 @@ const { publicRuntimeConfig } = getConfig()
 const config = {
   rpcUrl: publicRuntimeConfig.RPC_URL,
   contractAddresses: publicRuntimeConfig.TINLAKE_ADDRESSES && JSON.parse(publicRuntimeConfig.TINLAKE_ADDRESSES),
-  nftDataDefinition: publicRuntimeConfig.NFT_DATA_DEFINITION && JSON.parse(publicRuntimeConfig.NFT_DATA_DEFINITION)
+  nftDataDefinition: publicRuntimeConfig.NFT_DATA_DEFINITION && JSON.parse(publicRuntimeConfig.NFT_DATA_DEFINITION),
+  transactionTimeout: publicRuntimeConfig.TRANSCATION_TIMEOUT
 };
 
 if (!config.nftDataDefinition) {
