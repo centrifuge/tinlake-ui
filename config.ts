e42c2ef9cec48713f597ec7230e7683882697fec
ilinzweilin <alina@centrifuge.io>
2019-11-06 12:51:27 +0100

    remove logs

--- config.ts
+++ config.ts
@@ -12,7 +12,6 @@ const config = {
   isDemo: publicRuntimeConfig.ENV && (publicRuntimeConfig.ENV === 'demo'),
   network: publicRuntimeConfig.RPC_URL && networkUrlToName(publicRuntimeConfig.RPC_URL)
 };
-console.log("url", config.etherscanUrl)
 if (!config.nftDataDefinition) {
   throw new Error('Missing env NFT_DATA_DEFINITION');
 }
