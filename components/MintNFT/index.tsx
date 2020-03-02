c67e5aa377f6e85c9ae7f101adf4fdb13fe3de06
ilinzweilin <alina@centrifuge.io>
2020-03-02 16:01:25 +0100

    fix: format

--- components/MintNFT/index.tsx
+++ components/MintNFT/index.tsx
@@ -43,7 +43,7 @@ class MintNFT extends React.Component<Props, State> {
   }
 
   mint = async () => {
-    const { referenceId, assetType, amount } = this.state;
+    const { referenceId, assetType, amount, tokenId } = this.state;
    
     {
       this.setState({ is: 'loading' });
@@ -51,7 +51,7 @@ class MintNFT extends React.Component<Props, State> {
         await authTinlake();
         const base = displayToBase(baseToDisplay(amount, 2), 2);
         const res = await this.props.tinlake.mintNFT(
-          this.props.tinlake.ethConfig.from, this.state.tokenId, referenceId, base, assetType);
+          this.props.tinlake.ethConfig.from, tokenId, referenceId, base, assetType);
         if (res.status === SUCCESS_STATUS && res.events[0].event.name === 'Transfer') {
           this.setState({ is: 'success' });
         } else {
