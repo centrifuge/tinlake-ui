c3cd917518c9b0cfa75618fb01dd31180d1a83cb
ilinzweilin <alina@centrifuge.io>
2020-03-03 18:29:23 +0100

    feature: handle closed loans

--- containers/Loan/View/index.tsx
+++ containers/Loan/View/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Tinlake from 'tinlake';
 import { LoansState, loadLoan } from '../../../ducks/loans';
-import { Box, Heading} from 'grommet';
+import { Box, Heading } from 'grommet';
 import { connect } from 'react-redux';
 import Alert from '../../../components/Alert';
 import LoanData from '../../../components/Loan/Data';
@@ -23,7 +23,7 @@ interface Props {
 
 // on state change tokenId --> load nft data for loan collateral
 class LoanView extends React.Component<Props> {
-  
+
   componentWillMount() {
     this.props.loanId && this.props.loadLoan!(this.props.tinlake, this.props.loanId);
   }
@@ -31,51 +31,51 @@ class LoanView extends React.Component<Props> {
   render() {
     const { loans, loanId, tinlake, auth } = this.props;
     const { loan, loanState } = loans!;
-    
+
     if (loanState === null || loanState === 'loading') { return null; }
     if (loanState === 'not found') {
       return <Alert margin="medium" type="error">
         Could not find loan {loanId}</Alert>;
     }
 
-    const hasAdminPermissions = auth.user && ( auth.user.permissions.canSetInterestRate || auth.user.permissions.canSetCeiling);
+    const hasAdminPermissions = auth.user && (auth.user.permissions.canSetInterestRate || auth.user.permissions.canSetCeiling);
     const hasBorrowerPermissions = auth.user && loan && (loan.ownerOf === auth.user.address);
+
     return <Box>
       <LoanData loan={loan!} />
+      {loan && loan.status !== 'closed' &&
+        <Box>
+          {hasAdminPermissions &&
+            <Box pad={{ horizontal: 'medium' }} margin={{ top: "large", bottom: "large" }} >
+              <Box gap="medium" align="start" margin={{ bottom: "medium" }} >
+                <Heading level="5" margin="none">Loan Settings</Heading>
+              </Box>
+              <Box direction="row">
+                {auth.user && auth.user.permissions.canSetInterestRate &&
+                  <LoanInterest loan={loan!} tinlake={tinlake}> </LoanInterest>
+                }
+                {auth.user && auth.user.permissions.canSetCeiling &&
+                  <LoanCeiling loan={loan!} tinlake={tinlake}> </LoanCeiling>
+                }
+              </Box>
+            </Box>
+          }
 
-      { hasAdminPermissions &&
-        <Box pad={{ horizontal: 'medium' }} margin={{ top: "large", bottom: "large" }} >
-          <Box gap="medium" align="start"  margin={{ bottom: "medium" }} >
-            <Heading level="5" margin="none">Loan Settings</Heading>
-          </Box>
-          <Box direction="row">
-            { auth.user && auth.user.permissions.canSetInterestRate && 
-              <LoanInterest loan={loan!} tinlake={tinlake}> </LoanInterest>
-            }
-            { auth.user && auth.user.permissions.canSetCeiling && 
-             <LoanCeiling loan={loan!} tinlake={tinlake}> </LoanCeiling>
-            }
-          </Box>
+          {hasBorrowerPermissions &&
+            <Box pad={{ horizontal: 'medium' }} margin={{ top: "large", bottom: "large" }} >
+              <Box gap="medium" align="start" margin={{ bottom: "medium" }} >
+                <Heading level="5" margin="none">Borrow / Repay </Heading>
+              </Box>
+              <Box direction="row">
+                <LoanBorrow loan={loan!} tinlake={tinlake}> </LoanBorrow>
+                <LoanRepay loan={loan!} tinlake={tinlake}> </LoanRepay>
+              </Box>
+            </Box>
+          }
         </Box>
       }
-
-
-      { hasBorrowerPermissions &&
-       <Box pad={{ horizontal: 'medium' }} margin={{ top: "large", bottom: "large" }} >
-        <Box gap="medium" align="start"  margin={{ bottom: "medium" }} >
-          <Heading level="5" margin="none">Borrow / Repay </Heading>
-        </Box>
-        <Box direction="row">  
-          <LoanBorrow loan={loan!} tinlake={tinlake}> </LoanBorrow>
-          <LoanRepay loan={loan!} tinlake={tinlake}> </LoanRepay>
-        </Box>
-      </Box>
-      }
-
-      { loan && loan.nft && <NftData data={loan.nft} authedAddr={tinlake.ethConfig.from} /> }
-     
+      {loan && loan.nft && <NftData data={loan.nft} authedAddr={tinlake.ethConfig.from} />}
     </Box>
-
   }
 }
 
