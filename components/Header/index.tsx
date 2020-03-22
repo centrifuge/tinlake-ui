0469ede2366927329a1ccafa3a4b6973c5501b95
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-03-22 17:11:06 -0400

    Point navbar to npm package

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -8,9 +8,8 @@ import { formatAddress } from '../../utils/formatAddress';
 import config from '../../config';
 import { authTinlake } from '../../services/tinlake';
 import Router from 'next/router';
-//@ts-ignore
-//import { NavBar } from '@centrifuge/axis-nav-bar';
-import { NavBar} from './navbar';
+import { NavBar } from '@centrifuge/axis-nav-bar';
+
 
 const { isDemo } = config;
 export interface MenuItem {
