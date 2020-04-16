1316369dde16e1cbc92e0b5049df00e0496c1ecb
ilinzweilin <alina@centrifuge.io>
2020-04-16 18:23:16 +0200

    fix: format

--- pages/investments/investor.tsx
+++ pages/investments/investor.tsx
@@ -10,50 +10,50 @@ import Auth from '../../components/Auth';
 import Alert from '../../components/Alert';
 
 interface Props {
-    investorAddress: string;
+  investorAddress: string;
 }
 
 class InvestorPage extends React.Component<Props> {
-    static async getInitialProps({ query }: any) {
-        return { investorAddress: query.investorAddress };
-    }
+  static async getInitialProps({ query }: any) {
+    return { investorAddress: query.investorAddress };
+  }
 
-    render() {
-        const { investorAddress } = this.props;
-        return <Box align="center" pad={{ horizontal: "small" }}>
-            <Header
-                selectedRoute={'/investments/investor'}
-                menuItems={menuItems}
-            />
-            <Box
-                justify="center"
-                direction="row"
-            >
-                <Box width="xlarge" >
-                    <WithTinlake render={tinlake =>
-                        <Auth tinlake={tinlake} waitForAuthentication waitForAuthorization
-                            render={auth => auth && auth.state === 'loaded' && auth.user ?
-                                <Box>
-                                    <SecondaryHeader>
-                                        <Box direction="row" gap="small" align="center">
-                                            <BackLink href={'/investments'} />
-                                            <Box direction="row" gap="small" align="center"> 
-                                                <Heading level="3">Investor Details </Heading>
-                                                <Text style={{color: '#808080'}}> for address: {investorAddress}</Text>
-                                            </Box>
-                                        
-                                        </Box>
-                                    </SecondaryHeader>
-                                    <InvestorView investorAddress={investorAddress} tinlake={tinlake} auth={auth} />
-                                </Box>
-                                :
-                                <Alert margin="medium" type="error">
-                                    Please authenticate to access this page </Alert>
-                            } />
-                    } />
+  render() {
+    const { investorAddress } = this.props;
+    return <Box align="center" pad={{ horizontal: "small" }}>
+      <Header
+        selectedRoute={'/investments/investor'}
+        menuItems={menuItems}
+      />
+      <Box
+        justify="center"
+        direction="row"
+      >
+        <Box width="xlarge" >
+          <WithTinlake render={tinlake =>
+            <Auth tinlake={tinlake} waitForAuthentication waitForAuthorization
+              render={auth => auth && auth.state === 'loaded' && auth.user ?
+                <Box>
+                  <SecondaryHeader>
+                    <Box direction="row" gap="small" align="center">
+                      <BackLink href={'/investments'} />
+                      <Box direction="row" gap="small" align="center">
+                        <Heading level="3">Investor Details </Heading>
+                        <Text style={{ color: '#808080' }}> for address: {investorAddress}</Text>
+                      </Box>
+
+                    </Box>
+                  </SecondaryHeader>
+                  <InvestorView investorAddress={investorAddress} tinlake={tinlake} auth={auth} />
                 </Box>
-            </Box>
-        </Box>;
-    }
+                :
+                <Alert margin="medium" type="error">
+                  Please authenticate to access this page </Alert>
+              } />
+          } />
+        </Box>
+      </Box>
+    </Box>;
+  }
 }
 export default InvestorPage;
