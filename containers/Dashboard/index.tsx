545c70f25ad3d6916c3970be2e06b4d853660bbb
ilinzweilin <alina@centrifuge.io>
2020-05-20 09:25:05 +0200

    fix: show fresh pools deployments in pool list

--- containers/Dashboard/index.tsx
+++ containers/Dashboard/index.tsx
@@ -15,7 +15,6 @@ class Dashboard extends React.Component<Props> {
   componentDidMount() {
     const { loadPools } = this.props;
     loadPools && loadPools();
-
   }
 
   render() {
@@ -28,7 +27,7 @@ class Dashboard extends React.Component<Props> {
             message={'Loading...'}
           />
         ) : (
-          pools.data &&
+          pools.data && 
             <Box basis={'full'}>
               <Box margin={{ top: 'large', bottom: 'medium' }} direction="row">
                 <Box gap="large" style={{ minWidth: 130 }}>
