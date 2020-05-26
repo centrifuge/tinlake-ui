7c2176dc557797f97f888e6c827af067cf8ba687
ilinzweilin <alina@centrifuge.io>
2020-05-26 11:51:12 +0200

    fix: display design

--- components/Investment/TrancheMetric/index.tsx
+++ components/Investment/TrancheMetric/index.tsx
@@ -3,8 +3,8 @@ import { Box, Heading } from 'grommet';
 import { Investor } from 'tinlake';
 import { TrancheType } from '../../../services/tinlake/actions';
 import DashboardMetric from '../../DashboardMetric';
-import { Erc20Widget } from '../../../components/erc20-widget';
 import DAI from '../../../static/dai.json';
+import ERC20Display from '../../ERC20Display';
 
 interface Props {
   investor: Investor;
@@ -20,26 +20,29 @@ class TrancheMetric extends React.Component<Props> {
     return <Box margin="none">
       <Box>
         <Heading level="4" margin={{ bottom: 'medium' }}>Investment overview</Heading>
-        <Box direction="row" >
-
+        <Box direction="row" style={{ fontSize: '1.3em'}}>
+        <Box basis={"1/3"}>
             <DashboardMetric label="Investor token balance">
-              <Erc20Widget value={tokenBalance ? tokenBalance.toString() : '0'} tokenData={tokenData} precision={18}  />
+              <ERC20Display value={tokenBalance ? tokenBalance.toString() : '0'} tokenMetas={tokenData} precision={18} />
             </DashboardMetric>
-
+            </Box>
         </Box>
       </Box>
 
       <Box margin={{ top: 'medium' }}>
         <Heading level="4" margin={{ bottom: 'medium' }}>Invest / Redeem allowance</Heading>
-        <Box direction="row" >
+        <Box direction="row" style={{ fontSize: '1.3em' }} >
 
+          <Box basis={"1/3"}>
             <DashboardMetric label="Investment limit">
-              <Erc20Widget value={maxSupply ? maxSupply.toString() : '0'} tokenData={DAI} precision={18} />
+              <ERC20Display value={maxSupply ? maxSupply.toString() : '0'} tokenMetas={DAI} precision={18} />
             </DashboardMetric>
-
+            </Box>
+            <Box basis={"1/3"}>
             <DashboardMetric label="Redeem limit">
-             <Erc20Widget value={maxRedeem ? maxRedeem.toString() : '0'} tokenData={tokenData} precision={18} />
+              <ERC20Display value={maxRedeem ? maxRedeem.toString() : '0'} tokenMetas={tokenData} precision={18} />
             </DashboardMetric>
+            </Box>
 
         </Box>
       </Box>
