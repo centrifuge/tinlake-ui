f2635e9bc63e1888a403311cc1a847199aea74cd
Charly <charly@centrifuge.io>
2020-05-28 18:34:46 +0200

    build

--- components/Investment/TrancheMetric/index.tsx
+++ components/Investment/TrancheMetric/index.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { Box, Heading } from 'grommet';
 import { baseToDisplay, Investor } from 'tinlake';
 import DashboardMetric from '../../DashboardMetric';
-import DAI from '../../../static/dai.json';
+// import DAI from '../../../static/dai.json';
 import BN from 'bn.js';
 import { Tranche } from '../../../services/tinlake/actions';
-import ERC20Display from '../../ERC20Display';
+// import ERC20Display from '../../ERC20Display';
 
 interface Props {
   investor: Investor;
@@ -23,8 +23,11 @@ class TrancheMetric extends React.Component<Props> {
   }
 
   render() {
-    const {  investor, tranche, isAdmin } = this.props;
-    const { maxSupply, maxRedeem, tokenBalance } = investor[tranche.type];
+    const {
+      // investor,
+      tranche,
+      isAdmin } = this.props;
+    // const { maxSupply, maxRedeem, tokenBalance } = investor[tranche.type];
     const priceLabel = `Current ${tranche.token} Price`;
     const investmentValueLabel = `${tranche.token} Investment Value`;
     const availableTokenLabel = `${tranche.token} available to redeem in tranche`;
@@ -35,14 +38,17 @@ class TrancheMetric extends React.Component<Props> {
         <Box direction="row" >
 
             <DashboardMetric label={isAdmin ? 'Connected Address Token Balance' : 'Investor Address Token Balance'}>
-              <ERC20Display value={tokenBalance ? baseToDisplay(tokenBalance, 18) : '0'} tokenMetas={tranche.tokenData} precision={18}  />
+              // TODO: ERC20 typing for BN | string constraints
+              {/*<ERC20Display value={tokenBalance ? tokenBalance : '0'} tokenMetas={tranche.tokenData} precision={18}  />*/}
             </DashboardMetric>
 
           <DashboardMetric label={priceLabel}>
-            <ERC20Display value={tranche.tokenPrice ? baseToDisplay(tranche.tokenPrice, 27) : '0'} tokenMetas={DAI} precision={27} />
+            // TODO: figure out right precision
+            {/*<ERC20Display value={tranche.tokenPrice ? tranche.tokenPrice : '0'} tokenMetas={DAI} precision={27} />*/}
           </DashboardMetric>
           <DashboardMetric label={investmentValueLabel}>
-            <ERC20Display value={tokenBalance && tranche.tokenPrice ? (Number(baseToDisplay(tranche.tokenPrice, 27)) * Number(baseToDisplay(tokenBalance, 18))).toString() : '0'} tokenMetas={DAI} precision={18} />
+            // TODO: figure out right precision
+            {/*<ERC20Display value={tokenBalance && tranche.tokenPrice ? (Number(baseToDisplay(tranche.tokenPrice, 27)) * Number(baseToDisplay(tokenBalance, 18))).toString() : '0'} tokenMetas={DAI} precision={18} />*/}
           </DashboardMetric>
 
         </Box>
@@ -53,15 +59,18 @@ class TrancheMetric extends React.Component<Props> {
         <Box direction="row" >
 
             <DashboardMetric label={isAdmin ? 'Admin set Investment Limit' : 'Investment Limit'}>
-              <ERC20Display value={maxSupply ? baseToDisplay(maxSupply, 18) : '0'} tokenMetas={DAI} precision={18} />
+              // TODO: ERC20 typing for BN | string constraints
+              {/*<ERC20Display value={maxSupply ? maxSupply : '0'} tokenMetas={DAI} precision={18} />*/}
             </DashboardMetric>
 
             <DashboardMetric label={isAdmin ? 'Admin set Redeem Limit' : 'Redeem Limit'}>
-             <ERC20Display value={ isAdmin ? maxRedeem && baseToDisplay(maxRedeem, 18) || '' : tokenBalance && maxRedeem && this.checkInvestorRedeemLimit(tokenBalance, maxRedeem) || ''} tokenMetas={tranche.tokenData} precision={18} />
+              // TODO: ERC20 typing for BN | string constraints
+              {/*<ERC20Display value={ isAdmin ? maxRedeem && maxRedeem || '' : tokenBalance && maxRedeem && this.checkInvestorRedeemLimit(tokenBalance, maxRedeem) || ''} tokenMetas={tranche.tokenData} precision={18} />*/}
             </DashboardMetric>
 
           { isAdmin && <DashboardMetric label={availableTokenLabel}>
-            <ERC20Display value={ tranche.availableFunds ? tranche.availableFunds && baseToDisplay(tranche.availableFunds, 18) : '0'} tokenMetas={tranche.tokenData} precision={18} />
+            // TODO: ERC20 typing for BN | string constraints
+            {/*<ERC20Display value={ tranche.availableFunds ? tranche.availableFunds && tranche.availableFunds : '0'} tokenMetas={tranche.tokenData} precision={18} />*/}
           </DashboardMetric>}
         </Box>
       </Box>
