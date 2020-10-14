395ae9c77ff78adfaaa26a7ccbda83a7abce2f07
Jeroen Offerijns <jeroen@offerijns.com>
2020-10-14 15:34:21 +0200

    feat: set up score card on asset detail page

--- components/Loan/Data/index.tsx
+++ components/Loan/Data/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
-import { Box, FormField, TextInput } from 'grommet'
-import { baseToDisplay, feeToInterestRate, Loan } from '@centrifuge/tinlake-js'
+import { Box, Heading, FormField, TextInput, Table, TableBody, TableRow, TableCell } from 'grommet'
+import { baseToDisplay, feeToInterestRate } from '@centrifuge/tinlake-js'
+import { Loan } from '@centrifuge/tinlake-js-v3'
 import NumberInput from '../../NumberInput'
+import { addThousandsSeparators } from '../../../utils/addThousandsSeparators'
+import { toPrecision } from '../../../utils/toPrecision'
 
 interface Props {
   loan: Loan
@@ -15,45 +18,79 @@ class LoanData extends React.Component<Props> {
   render() {
     const { loanId, debt, principal, interestRate, status } = this.props.loan
     return (
-      <Box>
-        <Box direction="row" gap="medium">
-          <FormField label="Asset ID">
-            <TextInput value={loanId} disabled />
-          </FormField>
-          <FormField label="Status">
-            <TextInput value={status} disabled />
-          </FormField>
-          {(this.props.loan as any).riskGroup && (
-            <FormField label="Risk group">
-              <TextInput value={(this.props.loan as any).riskGroup} disabled />
+      <Box direction="row" gap="large">
+        <Box basis="2/3">
+          <Box direction="row" gap="medium">
+            <FormField label="Asset ID">
+              <TextInput value={loanId} disabled />
             </FormField>
-          )}
-        </Box>
+            <FormField label="Status">
+              <TextInput value={status} disabled />
+            </FormField>
+          </Box>
 
-        <Box direction="row" gap="medium" margin={{ bottom: 'medium', top: 'large' }}>
-          <Box basis={'1/4'} gap="medium">
-            <FormField label="Available for Financing">
-              <NumberInput value={baseToDisplay(principal, 18)} suffix=" DAI" disabled precision={18} />
-            </FormField>
-          </Box>
-          <Box basis={'1/4'} gap="medium">
-            <FormField label="Outstanding">
-              <NumberInput value={baseToDisplay(debt, 18)} suffix=" DAI" precision={18} disabled />
-            </FormField>
-          </Box>
-          <Box basis={'1/4'} gap="medium">
-            <FormField label="Financing Fee">
-              <NumberInput value={feeToInterestRate(interestRate)} suffix="%" disabled />
-            </FormField>
-          </Box>
-          {this.props.loan.nft && (this.props.loan.nft as any).maturityDate && (
-            <Box basis={'1/4'} gap="medium">
-              <FormField label="Maturity Date">
-                <TextInput value={dateToYMD((this.props.loan.nft as any).maturityDate)} disabled />
+          <Box direction="row" gap="medium" margin={{ bottom: 'medium', top: 'large' }}>
+            <Box basis={'1/3'} gap="medium">
+              <FormField label="Available for Financing">
+                <NumberInput value={baseToDisplay(principal, 18)} suffix=" DAI" disabled precision={18} />
               </FormField>
             </Box>
-          )}
+            <Box basis={'1/3'} gap="medium">
+              <FormField label="Outstanding">
+                <NumberInput value={baseToDisplay(debt, 18)} suffix=" DAI" precision={18} disabled />
+              </FormField>
+            </Box>
+            {this.props.loan.nft && (this.props.loan.nft as any).maturityDate && (
+              <Box basis={'1/3'} gap="medium">
+                <FormField label="Maturity Date">
+                  <TextInput value={dateToYMD((this.props.loan.nft as any).maturityDate)} disabled />
+                </FormField>
+              </Box>
+            )}
+          </Box>
         </Box>
+        {(this.props.loan as any).riskGroup && (this.props.loan as any).scoreCard && (
+          <Box basis="1/3" pad="medium">
+            <Box direction="row" margin={{ top: '0', bottom: 'small' }}>
+              <Heading level="5" margin={'0'}>
+                Score Card
+              </Heading>
+            </Box>
+
+            <Table margin={{ bottom: 'small' }}>
+              <TableBody>
+                <TableRow>
+                  <TableCell scope="row">Risk group</TableCell>
+                  <TableCell style={{ textAlign: 'end' }}>{(this.props.loan as any).riskGroup}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">Advance rate</TableCell>
+                  <TableCell style={{ textAlign: 'end' }}>
+                    {addThousandsSeparators(
+                      toPrecision(baseToDisplay((this.props.loan as any).scoreCard.ceilingRatio, 25), 2)
+                    )}{' '}
+                    %
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">Financing fee</TableCell>
+                  <TableCell style={{ textAlign: 'end' }}>
+                    {toPrecision(feeToInterestRate(interestRate), 2)} %
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell scope="row">Recovery rate</TableCell>
+                  <TableCell style={{ textAlign: 'end' }}>
+                    {addThousandsSeparators(
+                      toPrecision(baseToDisplay((this.props.loan as any).scoreCard.recoveryRatePD, 25), 2)
+                    )}{' '}
+                    %
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Box>
+        )}
       </Box>
     )
   }
